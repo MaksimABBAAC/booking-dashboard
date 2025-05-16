df87103abc317de428b63236dea2e52031a8f66a
MaksimABBAAC <97898486+MaksimABBAAC@users.noreply.github.com>
2025-05-16 16:53:02 +0500

    Task add valid phone number

--- clients/models.py
+++ clients/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Client(models.Model):
-    number = models.CharField('Номер телефона', max_length=12, unique=True)
+    number = PhoneNumberField(region='RU', verbose_name="Телефон", unique=True)
     tg_id = models.IntegerField(blank=True, null=True, verbose_name="Telegram ID")
     
     class Meta:
