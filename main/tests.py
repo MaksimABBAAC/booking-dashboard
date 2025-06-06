c5c1b904d999d3fb2e8fe69d82f3c08bc24c14a0
MaksimABBAAC <97898486+MaksimABBAAC@users.noreply.github.com>
2025-06-06 20:40:25 +0500

    Task add test main

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class Test_index():
+    @pytest.mark.django_db
+    def test_index_view_status_code(self, client):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        client.login(username='testuser', password='testpassword')
+        url = reverse('main:index')
+        response = client.get(url)
+        assert response.status_code == 200
+        assert 'title' in response.context
+        assert response.context['title'] == 'Панель системы записи - Главная'
+        assert 'main/index.html' in (t.name for t in response.templates)
